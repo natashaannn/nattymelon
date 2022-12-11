09a30a25f0808e7039d602f8947008ce4a108e7f
Natasha Ann <35247553+natashaannn@users.noreply.github.com>
2022-12-12 00:00:58 +0800

    Updated walking animation

--- src/js/renderables/player.js
+++ src/js/renderables/player.js
@@ -16,15 +16,15 @@ class PlayerEntity extends Entity {
         // player can exit the viewport (jumping, falling into a hole, etc.)
         this.alwaysUpdate = true;
 
-        // max walking speed
-        this.body.setMaxVelocity(3, 5);
-        this.body.setFriction(0.4, 0);
+        // walking & jumping speed
+        this.body.setMaxVelocity(2.5, 2.5);
+        this.body.setFriction(0.4,0.4);
 
         // enable keyboard
         me.input.bindKey(me.input.KEY.LEFT,  "left");
         me.input.bindKey(me.input.KEY.RIGHT, "right");
-        me.input.bindKey(me.input.KEY.DOWN, "down");
-        me.input.bindKey(me.input.KEY.UP, "up");
+        me.input.bindKey(me.input.KEY.UP,    "up");
+        me.input.bindKey(me.input.KEY.DOWN,  "down");
 
         // set a renderable
         this.renderable = game.texture.createAnimationFromName([
@@ -42,38 +42,33 @@ class PlayerEntity extends Entity {
             "avatar-right.png",
         ]);
 
-        // define standing animation
-        this.renderable.addAnimation ("stand",  [
-            { name: "avatar-front.png" }, 
-        ]);
-
         // define a basic walking animation (using all frames)
-        this.renderable.addAnimation ("walk-front",  [
+        this.renderable.addAnimation ("walk_down",  [
             { name: "avatar-front.png", delay: 100 }, 
             { name: "avatar-front-walk-left.png", delay: 100 }, 
             { name: "avatar-front-walk-right.png", delay: 100 }
         ]);
 
-        this.renderable.addAnimation ("walk-left",  [
+        this.renderable.addAnimation ("walk_left",  [
             { name: "avatar-left.png", delay: 100 }, 
             { name: "avatar-left-walk-left.png", delay: 100 }, 
             { name: "avatar-left-walk-right.png", delay: 100 }
         ]);
 
-        this.renderable.addAnimation ("walk-right",  [
+        this.renderable.addAnimation ("walk_right",  [
             { name: "avatar-right.png", delay: 100 }, 
             { name: "avatar-right-walk-left.png", delay: 100 }, 
             { name: "avatar-right-walk-right.png", delay: 100 }
         ]);
 
-        this.renderable.addAnimation ("walk-back",  [
+        this.renderable.addAnimation ("walk_up",  [
             { name: "avatar-back.png", delay: 100 }, 
             { name: "avatar-back-walk-left.png", delay: 100 }, 
             { name: "avatar-back-walk-right.png", delay: 100 }
         ]);
 
         // set as default
-        this.renderable.setCurrentAnimation("stand");
+        this.renderable.setCurrentAnimation("walk_down");
     }
 
     /**
@@ -81,29 +76,35 @@ class PlayerEntity extends Entity {
      */
     update(dt) {
 
-        if (me.input.isKeyPressed('left')) {
+        if (me.input.isKeyPressed("left")) {
+            // update the entity velocity
             this.body.force.x = -this.body.maxVel.x;
-            if (!this.renderable.isCurrentAnimation("walk-left")) {
-                this.renderable.setCurrentAnimation("walk-left");
+            if (!this.renderable.isCurrentAnimation("walk_left")) {
+                this.renderable.setCurrentAnimation("walk_left");
             }
-        } else if (me.input.isKeyPressed('right')) {
+        } else if (me.input.isKeyPressed("right")) {
+            // update the entity velocity
             this.body.force.x = this.body.maxVel.x;
-            if (!this.renderable.isCurrentAnimation("walk-right")) {
-                this.renderable.setCurrentAnimation("walk-right");
-            }
-        } else if (me.input.isKeyPressed('up')) {
-            this.body.force.y = -this.body.maxVel.y;
-            if (!this.renderable.isCurrentAnimation("walk-back")) {
-                this.renderable.setCurrentAnimation("walk-back");
-            }
-        } else if (me.input.isKeyPressed('down')) {
-            this.body.force.y = this.body.maxVel.y;
-            if (!this.renderable.isCurrentAnimation("walk-front")) {
-                this.renderable.setCurrentAnimation("walk-front");
+            if (!this.renderable.isCurrentAnimation("walk_right")) {
+                this.renderable.setCurrentAnimation("walk_right");
             }
         } else {
-            // change to the standing animation
-            this.renderable.setCurrentAnimation("stand");
+            this.body.force.x = 0;
+        }
+        if (me.input.isKeyPressed("up")) {
+            // update the entity velocity
+            this.body.force.y = -this.body.maxVel.y;
+            if (!this.renderable.isCurrentAnimation("walk_up") && this.body.vel.x === 0) {
+                this.renderable.setCurrentAnimation("walk_up");
+            }
+        } else if (me.input.isKeyPressed("down")) {
+            // update the entity velocity
+            this.body.force.y = this.body.maxVel.y;
+            if (!this.renderable.isCurrentAnimation("walk_down") && this.body.vel.x === 0) {
+                this.renderable.setCurrentAnimation("walk_down");
+            }
+        } else {
+            this.body.force.y = 0;
         }
 
         // check if we moved (an "idle" animation would definitely be cleaner)
